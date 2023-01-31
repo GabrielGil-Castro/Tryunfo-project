e7bbced7b8853fd24b0948b4705eca54d298ab6d
GabrielGil-Castro <gsgc05@gmail.com>
2023-01-31 09:55:59 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  /*  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: '',
+    hasTrunfo: '',
+    isSaveButtonDisabled: '',
+    onInputChange: '',
+    onSaveButtonClick: '',
+  }; */
+
   render() {
     return (
       <div>
         <h1>Tryunfo</h1>
         <Form />
+        <Card />
       </div>
     );
   }
